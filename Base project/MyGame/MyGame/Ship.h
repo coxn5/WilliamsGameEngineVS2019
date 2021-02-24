c24b82fbbd196af7c8cd015e32d5d0255c262edb
Nora Cox <coxn@rsd407.org>
2021-02-23 19:59:01 -0800

    Update to Part 4. Working on Part 5 next.

---
+++ Base project/MyGame/MyGame/Ship.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Engine/GameEngine.h"
+
+class Ship : public GameObject
+{
+public:
+	//Creates our ship.
+	Ship();
+
+	//Functions overridden from GameObect:
+	void draw();
+	void update(sf::Time& elapsed);
+private:
+	sf::Sprite sprite_;
+	int fireTimer_ = 0;
+};
+
+typedef std::shared_ptr<Ship> ShipPtr;
