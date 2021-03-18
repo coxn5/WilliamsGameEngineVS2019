022883ae47612fbe96af1af2044df0b99f813d72
Nora Cox <coxn@rsd407.org>
2021-03-18 10:25:21 -0700

    I need a bit of help on the Explosion part.

--- Base project/MyGame/MyGame/Explosion.cpp
+++ Base project/MyGame/MyGame/Explosion.cpp
@@ -1,9 +1,10 @@
 #include "Explosion.h"
 
+
+
 Explosion::Explosion(sf::Vector2f pos)
 	: AnimatedSprite(pos)
 {
-	
 	AnimatedSprite::setTexture(GAME.getTexture("Resources/explosion-spritesheet.png"));
 	SetUpExplosionAnimation();
 	playAnimation("explosion", AnimationMode::OnceForwards);
