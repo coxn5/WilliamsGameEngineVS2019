f7fd921f961226cb56f4abb0f0a93f48d6b4c1cb
Nora Cox <coxn@rsd407.org>
2021-05-20 12:43:42 -0700

    Just some adjusting, trying to come up with am idea for my Game Design Document.

--- Base project/MyGame/MyGame/GameOverMessage.cpp
+++ Base project/MyGame/MyGame/GameOverMessage.cpp
@@ -6,11 +6,11 @@ GameOverMessage::GameOverMessage(int score)
 {
 	text_.setFont(GAME.getFont("Resources/Courneuf-Regular.ttf"));
 	text_.setPosition(sf::Vector2f(50.0f, 50.0f));
-	text_.setCharacterSize(48);
-	text_.setFillColor(sf::Color::Red);
+	text_.setCharacterSize(35);
+	text_.setFillColor(sf::Color::Blue);
 
 	std::stringstream stream;
-	stream << "GAME OVER\n\nYOUR SCORE: " << score << "\n\nPRESS ENTER TO CONTINUE";
+	stream << "Good Try\n\nYour Score is: " << score << "\n\nPress Enter to Continue";
 	text_.setString(stream.str());
 }
 
---
+++ Base project/MyGame/MyGame/Notes.cpp
@@ -0,0 +1,7 @@
+/*
+App Design Document Ideas
+
+Racing game. -Have coins or something to make you go faster - will probably need a scrolling background or a finish line.
+
+
+*/
